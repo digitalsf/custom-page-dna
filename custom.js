88840572b50014c254dd6f47fbecf58dfd83e652
Mark Truitt <matruitt30@gmail.com>
2022-01-12 15:11:49 -0500

    Updates

--- custom.js
+++ custom.js
@@ -1,38 +1,39 @@
 window.addEventListener('load', function () {
-    const container = document.querySelector('#item_details_view')
     
+    const container = document.querySelector('#item_details_view')
+    const allItems = document.querySelector('#all_items_view')
+
     let myObserver = new ResizeObserver(entries => {
         entries.forEach(entry => {
-            //console.log(entry)
-            //container.style.height = 'auto'
-            console.log(container.clientHeight)
+
             let forms = document.getElementsByTagName('form')
+
             for(form of forms) {
                 form.style.height = 'auto'
             }
         
             let divs = document.getElementsByTagName('div')
+            
             for(div of divs) {
                 div.style.height = 'auto'
             }
-        
-            let itemContainer = document.querySelector('#all_items_view')
-            let itemContainerHeight = itemContainer.clientHeight
+            
             let mainContainer = document.querySelector('.main')
-            let itemDetails = document.getElementById('item_details_view')
-            let itemDetailsHeight = itemDetails.clientHeight
+            let itemContainer = document.querySelector('#all_items_view').clientHeight
+            let itemDetails = document.getElementById('item_details_view').clientHeight
+            
             if(itemContainerHeight != 0) {
                 //mainContainer.style.height = `${itemContainerHeight}px`
                 mainContainer.style.height = 'auto'
             } else {
                 mainContainer.style.height = `${itemDetailsHeight}px`
             }
-
+            mainContainer.style.height = 'auto'
             console.log('fired')
         })
     })
     
     //myObserver.observe(container)
     myObserver.observe(container)
-
+    myObserver.observe(allItems)
 })
