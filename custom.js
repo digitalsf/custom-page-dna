57d8ee995b887ebd8cbdf2f3e7c81b250afc44f2
Mark Truitt <matruitt30@gmail.com>
2022-01-12 11:38:58 -0500

    Update

--- custom.js
+++ custom.js
@@ -1,9 +1,11 @@
-const divs = document.getElementsByTagName('div')
-
-const myObserver = new ResizeObserver(entries => {
+let myObserver = new ResizeObserver(entries => {
     entries.forEach(entry => {
         entry.style.height = 'auto'
     })
 })
 
+const divs = document.querySelector('div')
+const forms = document.getElementsByTagName('form')
+
 myObserver.observe(divs)
+myObserver.observe(forms)
