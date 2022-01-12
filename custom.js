7686a0424e8dee2e14af7ec24ae3e99304bc9c25
Mark Truitt <matruitt30@gmail.com>
2022-01-12 11:23:25 -0500

    Updates

--- custom.js
+++ custom.js
@@ -1,19 +1,9 @@
 const divs = document.getElementsByTagName('div')
-const forms = document.getElementsByTagName('form')
 
-function divResizeHeight() {
-    setTimeout(function () {
-        console.log('Resize detected')
-    for(div of divs) {
-        div.style.height = 'auto'
-    }
+const myObserver = new ResizeObserver(entries => {
+    entries.forEach(entry => {
+        entry.style.height = 'auto'
+    })
+})
 
-    for(form of forms) {
-        form.style.height = 'auto'
-    }
-    }, 1000)
-}
-
-divResizeHeight()
-
-new ResizeObserver(divResizeHeight).observe(item_details_view)
+myObserver.observe(divs)
