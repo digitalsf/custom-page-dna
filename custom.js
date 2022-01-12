b3d3fcc965cb22a83319f541c94441d36d9f0e34
Mark Truitt <matruitt30@gmail.com>
2022-01-12 12:40:00 -0500

    Update

--- custom.js
+++ custom.js
@@ -1,25 +1,27 @@
-const container = document.querySelector('.main')
+window.addEventListener('load', function () {
+    const container = document.querySelector('.main')
 
-let myObserver = new ResizeObserver(entries => {
-    entries.forEach(entry => {
-        //console.log(entry)
-        container.style.height = 'auto'
-    })
-
-    let forms = document.getElementsByTagName('form')
-    for(form of forms) {
-        form.style.height = 'auto'
-    }
-
-    let divs = document.getElementsByTagName('div')
-    for(div of divs) {
-        div.style.height = 'auto'
-    }
-
-    let itemContainer = document.querySelector('#all_items_view')
-    let itemContainerHeight = itemContainer.clientHeight
+    let myObserver = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+            //console.log(entry)
+            container.style.height = 'auto'
+        })
     
-    itemContainer.style.height = itemContainerHeight
+        let forms = document.getElementsByTagName('form')
+        for(form of forms) {
+            form.style.height = 'auto'
+        }
+    
+        let divs = document.getElementsByTagName('div')
+        for(div of divs) {
+            div.style.height = 'auto'
+        }
+    
+        let itemContainer = document.querySelector('#all_items_view')
+        let itemContainerHeight = itemContainer.clientHeight
+        
+        itemContainer.style.height = itemContainerHeight
+    })
+    
+    myObserver.observe(container)    
 })
-
-myObserver.observe(container)
