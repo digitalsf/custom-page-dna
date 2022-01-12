f53ccab2b0175a799fa0a9e76e960b307e7e3585
Mark Truitt <matruitt30@gmail.com>
2022-01-12 14:54:28 -0500

    Update

--- custom.js
+++ custom.js
@@ -1,48 +1,37 @@
 window.addEventListener('load', function () {
-    const container = document.querySelector('.main')
-    const body = document.getElementsByTagName('body')
+    const container = document.querySelector('#item_details_view')
+    
     let myObserver = new ResizeObserver(entries => {
         entries.forEach(entry => {
             //console.log(entry)
-            container.style.height = 'auto'
-        })
-    
-        let forms = document.getElementsByTagName('form')
-        for(form of forms) {
-            form.style.height = 'auto'
-        }
-    
-        let divs = document.getElementsByTagName('div')
-        for(div of divs) {
-            div.style.height = 'auto'
-        }
-    
-        let itemContainer = document.querySelector('#all_items_view')
-        let itemContainerHeight = itemContainer.clientHeight
-        let mainContainer = document.querySelector('.main')
-        let itemDetails = document.getElementById('item_details_view')
-        let itemDetailsHeight = itemDetails.clientHeight
-        if(itemContainerHeight != 0) {
-            mainContainer.style.height = `${itemContainerHeight}px`
-        } else {
-            mainContainer.style.height = `${itemDetailsHeight}px`
-        }
+            //container.style.height = 'auto'
+            console.log(container.clientHeight)
+            let forms = document.getElementsByTagName('form')
+            for(form of forms) {
+                form.style.height = 'auto'
+            }
+        
+            let divs = document.getElementsByTagName('div')
+            for(div of divs) {
+                div.style.height = 'auto'
+            }
+        
+            let itemContainer = document.querySelector('#all_items_view')
+            let itemContainerHeight = itemContainer.clientHeight
+            let mainContainer = document.querySelector('.main')
+            let itemDetails = document.getElementById('item_details_view')
+            let itemDetailsHeight = itemDetails.clientHeight
+            if(itemContainerHeight != 0) {
+                mainContainer.style.height = `${itemContainerHeight}px`
+            } else {
+                mainContainer.style.height = `${itemDetailsHeight}px`
+            }
 
-        console.log('fired')
+            console.log('fired')
+        })
     })
     
     //myObserver.observe(container)
-    myObserver.observe(document.body)
+    myObserver.observe(container)
 
-    const observer = new MutationObserver(function(mutations_list) {
-        mutations_list.forEach(function(mutation) {
-            mutation.addedNodes.forEach(function(added_node) {
-                    console.log(added_node)
-                    console.log('child has been added');
-                    observer.disconnect();
-            });
-        });
-    });
-    
-    observer.observe(document.getElementsByTagName("form"), { subtree: false, childList: true });
 })
