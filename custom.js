e0c251563dfde19cc580583f889023ff938c3cc3
Mark Truitt <matruitt30@gmail.com>
2022-01-12 10:37:06 -0500

    Update

--- custom.js
+++ custom.js
@@ -1,5 +1,10 @@
+const divs = document.getElementsByTagName('div')
+
 function divResizeHeight() {
     console.log('Resize detected')
+    for(div of divs) {
+        div.style.height = 'auto'
+    }
 }
 
-new ResizeObserver(divResizeHeight).observe(item_details)
+new ResizeObserver(divResizeHeight).observe(item_details_view)
