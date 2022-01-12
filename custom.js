cb889a677d1e107851b4f3c0ca10fc42a53e43f4
Mark Truitt <matruitt30@gmail.com>
2022-01-12 10:42:23 -0500

    Updates

--- custom.js
+++ custom.js
@@ -1,10 +1,17 @@
 const divs = document.getElementsByTagName('div')
+const forms = document.getElementsByTagName('form')
 
 function divResizeHeight() {
     console.log('Resize detected')
+    for(form of forms) {
+        form.style.height = 'auto'
+    }
     for(div of divs) {
         div.style.height = 'auto'
     }
+
 }
+
+divResizeHeight()
 
 new ResizeObserver(divResizeHeight).observe(item_details_view)
