dabd520242d7d4a93e739266f984c8b3238ed26b
Mark Truitt <matruitt30@gmail.com>
2022-01-13 14:57:14 -0500

    Update

--- custom.js
+++ custom.js
@@ -28,13 +28,16 @@ window.addEventListener('load', function () {
     Observer.observe(container)
 
     window.hue=0.0;
-    window.disco_speed=24.0; // 4.0 = 1 css-hsl() hue point per second
+    window.disco_speed=8.0; // 4.0 = 1 css-hsl() hue point per second
     function discoLaserDanceRave () {
         jQuery('body').css('background-color','hsl('+window.hue+',100%,50%)');
         window.hue+=window.disco_speed/4;
         if (window.hue>360) window.hue=window.hue-360;
         setTimeout(discoLaserDanceRave,10);
     }
-    discoLaserDanceRave();
+    //discoLaserDanceRave();
 
+    if( $('.bundle_subitem_subgroupname').length ) {
+        $('.bundle_subitem_subgroupname:parent(tr)').before('</tbody></table><table class="bundle_subgroup_table"><tbody>');
+    }
 })
