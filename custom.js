bdfd9af3882bb49b1420722ace7194207ee4ee41
Mark Truitt <matruitt30@gmail.com>
2022-02-15 12:14:21 -0500

    Update
    Updated conditional for ResizeObserver

--- custom.js
+++ custom.js
@@ -2,31 +2,34 @@ window.addEventListener('load', function () {
     
     // Custom Product Start
     const container = document.querySelector('#item_details_view')
-
-    let Observer = new ResizeObserver(entries => {
-        entries.forEach(entry => {
-
-            let forms = document.getElementsByTagName('form')
-
-            for(form of forms) {
-                form.style.height = 'max-content'
-                form.style.overflow = 'auto'
-            }
-        
-            let divs = document.getElementsByTagName('div')
-            
-            for(div of divs) {
-                div.style.height = 'max-content'
-            }
-            
-            let mainContainer = document.querySelector('.main')            
-            
-            mainContainer.style.height = 'max-content'
-            
-        })
-    })
     
-    Observer.observe(container)
+    if (typeof(container) != 'undefined' && container != null)) {
+    
+        let Observer = new ResizeObserver(entries => {
+            entries.forEach(entry => {
+
+                let forms = document.getElementsByTagName('form')
+
+                for(form of forms) {
+                    form.style.height = 'max-content'
+                    form.style.overflow = 'auto'
+                }
+
+                let divs = document.getElementsByTagName('div')
+
+                for(div of divs) {
+                    div.style.height = 'max-content'
+                }
+
+                let mainContainer = document.querySelector('.main')            
+
+                mainContainer.style.height = 'max-content'
+
+            })
+        })
+
+        Observer.observe(container)
+    }
     // Custom Product End
     
      //$('.responsive_tabs-shell .before').appendTo('.header-links'); //move search to header bar
